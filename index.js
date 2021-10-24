1228aca26d24717e53777c4758dfd7eca2e485d2
ignacioND <ignacio.navarro@gmail.com>
2021-10-24 15:47:53 +0200

    Install and configure Expo
    - Check that everithing runs correctly
    - Install and configure Eslint

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * @format
  */
 
-import { AppRegistry } from "react-native";
-import Home from "./src/screens/home/Home";
+import { AppRegistry } from 'react-native';
+import Home from './src/screens/home/Home';
 
-import { name as appName } from "./app.json";
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => Home);
--- src/screens/recipes/Details.js
+++ src/screens/recipes/Details.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   Text,
   StyleSheet,
@@ -7,31 +7,29 @@ import {
   ScrollView,
   SafeAreaView,
   StatusBar,
-} from "react-native";
+} from 'react-native';
 
-import Colors from "../../theme/Colors";
-import Metrics from "../../theme/Metrics";
+import Colors from '../../theme/Colors';
+import Metrics from '../../theme/Metrics';
 
 const data = {
-  "_id": "52944",
-  "name": "Escovitch Fish",
-  "categoryId": "1",
-  "categoryName": "Fish",
-  "duration": 11,
-  "complexity": "Hard",
-  "people": 3,
-  "recommended": 1,
-  "ingredients": "2 eggs\r\n4 tomatoes\r\nsalt\r\npepper",
-  "description":
-      "Rinse fish; rub with lemon or lime, seasoned with salt and pepper or use your favorite seasoning. I used creole seasoning. Set aside or place in the oven to keep it warm until sauce is ready.\r\n\r\nIn large skillet heat oil over medium heat, until hot, add the fish, cook each side- for about 5-7 minutes until cooked through and crispy on both sides. Remove fish and set aside. Drain oil and leave about 2-3 tablespoons of oil\r\nAdd, bay leave, garlic and ginger, stir-fry for about a minute making sure the garlic does not burn\r\nThen add onion, bell peppers, thyme, scotch bonnet, sugar, all spice-continue stirring for about 2-3 minutes. Add vinegar, mix an adjust salt and pepper according to preference. Let it simmer for about 2 more minutes. \r\n\r\nDiscard bay leave, thyme spring and serve over fish with a side of this bammy. You may make the sauce about 2 days in advance.",
-  "photo": "https://www.themealdb.com/images/media/meals/1520084413.jpg",
+  _id: '52944',
+  name: 'Escovitch Fish',
+  categoryId: '1',
+  categoryName: 'Fish',
+  duration: 11,
+  complexity: 'Hard',
+  people: 3,
+  recommended: 1,
+  ingredients: '2 eggs\r\n4 tomatoes\r\nsalt\r\npepper',
+  description:
+      'Rinse fish; rub with lemon or lime, seasoned with salt and pepper or use your favorite seasoning. I used creole seasoning. Set aside or place in the oven to keep it warm until sauce is ready.\r\n\r\nIn large skillet heat oil over medium heat, until hot, add the fish, cook each side- for about 5-7 minutes until cooked through and crispy on both sides. Remove fish and set aside. Drain oil and leave about 2-3 tablespoons of oil\r\nAdd, bay leave, garlic and ginger, stir-fry for about a minute making sure the garlic does not burn\r\nThen add onion, bell peppers, thyme, scotch bonnet, sugar, all spice-continue stirring for about 2-3 minutes. Add vinegar, mix an adjust salt and pepper according to preference. Let it simmer for about 2 more minutes. \r\n\r\nDiscard bay leave, thyme spring and serve over fish with a side of this bammy. You may make the sauce about 2 days in advance.',
+  photo: 'https://www.themealdb.com/images/media/meals/1520084413.jpg',
 };
 
 export default class RecipeDetails extends Component {
-
   render() {
-
-    let imageUrl = "http://via.placeholder.com/640x360";
+    let imageUrl = 'http://via.placeholder.com/640x360';
     if (data && data.photo) {
       imageUrl = data.photo;
     }
@@ -50,13 +48,21 @@ export default class RecipeDetails extends Component {
               <Text style={styles.title}>{data.name}</Text>
               <View style={styles.properties}>
                 <View style={styles.cell}>
-                  <Text style={styles.cellText}>{data.duration} minutes</Text>
+                  <Text style={styles.cellText}>
+                    {data.duration}
+                    {' '}
+                    minutes
+                  </Text>
                 </View>
                 <View style={styles.cell}>
                   <Text style={styles.cellText}>{data.complexity}</Text>
                 </View>
                 <View style={styles.cell}>
-                  <Text style={styles.cellText}>{data.people} people</Text>
+                  <Text style={styles.cellText}>
+                    {data.people}
+                    {' '}
+                    people
+                  </Text>
                 </View>
               </View>
             </View>
@@ -79,38 +85,38 @@ const styles = StyleSheet.create({
   category: {
     color: Colors.darkGrey,
     fontSize: 10,
-    fontStyle: "normal",
-    fontWeight: "bold",
+    fontStyle: 'normal',
+    fontWeight: 'bold',
     letterSpacing: 1,
     lineHeight: 24,
-    textAlign: "center",
+    textAlign: 'center',
   },
   cell: {
-    alignItems: "center",
+    alignItems: 'center',
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
     marginRight: Metrics.smallSpace,
   },
   cellText: {
     color: Colors.mediumGrey,
     fontSize: 13,
-    fontStyle: "normal",
-    fontWeight: "300",
+    fontStyle: 'normal',
+    fontWeight: '300',
     letterSpacing: 0.5,
     lineHeight: 24,
   },
   container: {
-    alignItems: "stretch",
+    alignItems: 'stretch',
     backgroundColor: Colors.mainBackground,
     flex: 1,
-    justifyContent: "flex-start",
+    justifyContent: 'flex-start',
   },
   darkener: {
     backgroundColor: Colors.black30,
     bottom: 0,
     left: 0,
-    position: "absolute",
+    position: 'absolute',
     right: 0,
     top: 0,
     zIndex: 5,
@@ -124,13 +130,13 @@ const styles = StyleSheet.create({
     color: Colors.black,
     fontSize: 14,
     lineHeight: 24,
-    textAlign: "center",
+    textAlign: 'center',
   },
   imageContainer: {},
   infoBox: {
-    alignItems: "stretch",
-    alignSelf: "stretch",
-    justifyContent: "center",
+    alignItems: 'stretch',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
     paddingHorizontal: Metrics.baseSpace,
     paddingVertical: Metrics.smallSpace,
   },
@@ -139,23 +145,23 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   properties: {
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "flex-start",
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
   },
   recipeImage: {
     height: 192,
   },
   title: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   title: {
     color: Colors.black,
     fontSize: 16,
-    fontStyle: "normal",
-    fontWeight: "500",
+    fontStyle: 'normal',
+    fontWeight: '500',
     letterSpacing: 0.5,
     lineHeight: 24,
-    textAlign: "center",
+    textAlign: 'center',
   },
-})
+});
